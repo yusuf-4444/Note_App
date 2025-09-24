8ac442c34ddc19ed3b464279e9af2f4708ca43bc
Yusuf <youssefhamed374@gmail.com>
2025-09-24 15:31:55 -0700

    create show modal bottom sheet

--- lib/views/home_page_view.dart
+++ lib/views/home_page_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/widgets/note_bottom_sheet_widget.dart';
 import 'package:note_app/widgets/notes_body_widget.dart';
 
 class HomeView extends StatelessWidget {
@@ -8,7 +9,17 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadiusGeometry.circular(16),
+            ),
+            context: context,
+            builder: (context) {
+              return AddNoteBottomSheet();
+            },
+          );
+        },
         child: Icon(Icons.add),
       ),
       body: NotesViewBody(),
