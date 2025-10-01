38c210f99848ef143cc6b201c3c8e2b9aa0b34cc
yusuf <youssefhamed374@gmail.com>
2025-10-01 11:35:01 +0200

    add Add note bloc provider to colors list

--- lib/cubits/cubit/add_note_cubit.dart
+++ lib/cubits/cubit/add_note_cubit.dart
@@ -1,6 +1,6 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
-import 'package:meta/meta.dart';
 import 'package:note_app/constants.dart';
 import 'package:note_app/models/note_model.dart';
 
@@ -9,7 +9,10 @@ part 'add_note_state.dart';
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
 
+  Color color = Color(0x000000ff);
+
   addNote(NoteModel note) async {
+    note.color = color.toARGB32();
     emit(AddNoteLoading());
 
     try {
--- lib/widgets/color_list_view_widget.dart
+++ lib/widgets/color_list_view_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/cubits/cubit/add_note_cubit.dart';
 
 class ColorItem extends StatelessWidget {
   const ColorItem({super.key, required this.isActive, required this.color});
@@ -47,6 +49,7 @@ class _ColorsListViewState extends State<ColorsListView> {
           return GestureDetector(
             onTap: () {
               currentIndex = index;
+              BlocProvider.of<AddNoteCubit>(context).color = colors[index];
               setState(() {});
             },
             child: ColorItem(
